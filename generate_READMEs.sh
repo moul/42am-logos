e6cff4ccb6512ebf3254b600a6b1b15342252947
Manfred Touron <github@42.am>
2012-03-15 16:35:03 +0100

    removed svg support in markdown

--- generate_READMEs.sh
+++ generate_READMEs.sh
@@ -16,11 +16,11 @@ for dir in *; do
                     echo "<a href=\"/$ACCOUNT/$REPO/raw/master/$dir/$image\"><img src=\"/$ACCOUNT/$REPO/raw/master/$dir/$image\" width=\"400px\" /></a>"
                 fi
             done
-            for image in *.svg; do
-                if [ -f $image ]; then
-                    echo "<embed src=\"/$ACCOUNT/$REPO/raw/master/$dir/$image\" type=\"image/svg+xml\" width=\"400px\" />"
-                fi
-            done
+            #for image in *.svg; do
+            #    if [ -f $image ]; then
+            #        echo "<embed src=\"/$ACCOUNT/$REPO/raw/master/$dir/$image\" type=\"image/svg+xml\" width=\"400px\" />"
+            #    fi
+            #done
             echo
             ) > $pwd/$dir/README.md
     fi
