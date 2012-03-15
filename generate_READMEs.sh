6a54ab76849c2606c07b15936b3cce83dd0ab36b
Manfred Touron <github@42.am>
2012-03-15 16:10:43 +0100

    added READMEs generation script

---
+++ generate_READMEs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+ACCOUNT=42am
+REPO=logos
+
+pwd=$(pwd)
+for dir in *; do
+    if [ -d $pwd/$dir ]; then
+        cd $pwd/$dir
+        (
+            echo  "$dir"
+            echo "$dir" | sed "s/./=/g"
+            echo
+            for image in *; do
+                if [ -f $image ]; then
+                    echo "<a href=\"/$ACCOUNT/$REPO/raw/master/$dir/$image\"><img src=\"/$ACCOUNT/$REPO/raw/master/$dir/$image\" width=\"400px\" /></a>"
+                fi
+            done
+            echo
+            ) > $pwd/$dir/README.md
+    fi
+done
