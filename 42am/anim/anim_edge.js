762fa2830e9db190641fde2437cf5b3b09c86830
vik <v@42.am>
2012-03-15 20:22:15 +0100

    anim v1.2

--- 42am/anim/anim_edge.js
+++ 42am/anim/anim_edge.js
@@ -38,7 +38,7 @@ var symbols = {
             type:'image',
             rect:[364,167,29,111],
             fill:["rgba(0,0,0,0)",im+"2point.png"],
-            transform:[[-69,-23]]
+            transform:[[-70,-23]]
          },
          {
             id:'am',
@@ -65,7 +65,7 @@ var symbols = {
          "${__2point}": [
             ["style", "opacity", '0'],
             ["transform", "translateY", '-23px'],
-            ["transform", "translateX", '-69px']
+            ["transform", "translateX", '-70px']
          ],
          "${__2}": [
             ["transform", "translateY", '-373px'],
@@ -89,19 +89,19 @@ var symbols = {
          },
          timeline: [
             { id: "eid31", tween: [ "transform", "${__4}", "translateX", '-39px', { fromValue: '-39px'}], position: 1250, duration: 0 },
+            { id: "eid34", tween: [ "transform", "${__4}", "translateY", '-63px', { fromValue: '-356px'}], position: 250, duration: 1000, easing: "easeOutBounce" },
             { id: "eid36", tween: [ "transform", "${__2}", "translateY", '-80px', { fromValue: '-373px'}], position: 0, duration: 1000, easing: "easeOutBounce" },
-            { id: "eid35", tween: [ "transform", "${_am}", "translateY", '-170.826px', { fromValue: '-393px'}], position: 1000, duration: 500, easing: "easeInQuad" },
-            { id: "eid46", tween: [ "transform", "${_am}", "translateY", '-80px', { fromValue: '-170.826px'}], position: 1500, duration: 750, easing: "easeOutBounce" },
-            { id: "eid43", tween: [ "transform", "${_am}", "translateY", '-80px', { fromValue: '-80px'}], position: 2250, duration: 1750, easing: "easeOutBounce" },
-            { id: "eid49", tween: [ "style", "${__2point}", "opacity", '0', { fromValue: '0'}], position: 0, duration: 0, easing: "easeOutBounce" },
-            { id: "eid52", tween: [ "style", "${__2point}", "opacity", '1', { fromValue: '0.000000'}], position: 2250, duration: 500, easing: "easeOutBounce" },
+            { id: "eid44", tween: [ "transform", "${_am}", "rotateZ", '-64deg', { fromValue: '-56deg'}], position: 1000, duration: 500, easing: "easeInQuad" },
+            { id: "eid47", tween: [ "transform", "${_am}", "rotateZ", '0deg', { fromValue: '-64deg'}], position: 1500, duration: 750, easing: "easeOutElastic" },
             { id: "eid29", tween: [ "transform", "${__2}", "translateX", '-45px', { fromValue: '-45px'}], position: 1000, duration: 0 },
             { id: "eid39", tween: [ "transform", "${_am}", "translateX", '-87.842px', { fromValue: '-62.954px'}], position: 1000, duration: 500, easing: "easeInQuad" },
             { id: "eid45", tween: [ "transform", "${_am}", "translateX", '-64.952px', { fromValue: '-87.842px'}], position: 1500, duration: 750, easing: "easeOutBounce" },
             { id: "eid42", tween: [ "transform", "${_am}", "translateX", '-65px', { fromValue: '-64.952px'}], position: 2250, duration: 1750 },
-            { id: "eid44", tween: [ "transform", "${_am}", "rotateZ", '-64deg', { fromValue: '-56deg'}], position: 1000, duration: 500, easing: "easeInQuad" },
-            { id: "eid47", tween: [ "transform", "${_am}", "rotateZ", '0deg', { fromValue: '-64deg'}], position: 1500, duration: 750, easing: "easeOutElastic" },
-            { id: "eid34", tween: [ "transform", "${__4}", "translateY", '-63px', { fromValue: '-356px'}], position: 250, duration: 1000, easing: "easeOutBounce" }         ]
+            { id: "eid35", tween: [ "transform", "${_am}", "translateY", '-170.826px', { fromValue: '-393px'}], position: 1000, duration: 500, easing: "easeInQuad" },
+            { id: "eid46", tween: [ "transform", "${_am}", "translateY", '-80px', { fromValue: '-170.826px'}], position: 1500, duration: 750, easing: "easeOutBounce" },
+            { id: "eid43", tween: [ "transform", "${_am}", "translateY", '-80px', { fromValue: '-80px'}], position: 2250, duration: 1750, easing: "easeOutBounce" },
+            { id: "eid49", tween: [ "style", "${__2point}", "opacity", '0', { fromValue: '0'}], position: 0, duration: 0, easing: "easeOutBounce" },
+            { id: "eid52", tween: [ "style", "${__2point}", "opacity", '1', { fromValue: '0.000000'}], position: 2250, duration: 500, easing: "easeOutBounce" }         ]
       }
    }
 }
